781981dce8e4e11f6eaed4713e0dcd9d9c328f6f
ErickVioland <50378370+ErickVioland@users.noreply.github.com>
2019-08-04 17:31:59 -0400

    Add files via upload

---
+++ Proyecto_Algoritmos_sln/Proyecto_Algoritmos/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Proyecto_Algoritmos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNumeros = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.txtRes = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(16, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(632, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Promedios y Medianas de un Conjunto de Numeros";
+            // 
+            // txtNumeros
+            // 
+            this.txtNumeros.Location = new System.Drawing.Point(18, 102);
+            this.txtNumeros.Multiline = true;
+            this.txtNumeros.Name = "txtNumeros";
+            this.txtNumeros.Size = new System.Drawing.Size(556, 68);
+            this.txtNumeros.TabIndex = 2;
+            this.txtNumeros.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtNumeros_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(18, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(449, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ingrese los numeros que desea agregar al conjunto:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(580, 102);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 68);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Calcular:";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(18, 330);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.listBox2.Size = new System.Drawing.Size(150, 134);
+            this.listBox2.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(18, 307);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Pares:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(229, 307);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Impares:";
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(227, 330);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.listBox3.Size = new System.Drawing.Size(156, 134);
+            this.listBox3.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(424, 307);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Repetición:";
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.Location = new System.Drawing.Point(419, 330);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(246, 134);
+            this.listBox4.TabIndex = 11;
+            // 
+            // txtRes
+            // 
+            this.txtRes.Location = new System.Drawing.Point(18, 187);
+            this.txtRes.Multiline = true;
+            this.txtRes.Name = "txtRes";
+            this.txtRes.Size = new System.Drawing.Size(647, 117);
+            this.txtRes.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(677, 494);
+            this.Controls.Add(this.txtRes);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNumeros);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Promedios y Medianas de un Conjunto";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNumeros;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.TextBox txtRes;
+    }
+}
+
---
+++ Proyecto_Algoritmos_sln/Proyecto_Algoritmos/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_Algoritmos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private Dictionary<String, List<int>> Paridad(string[] lista)
+        {
+            int num;
+            List<int> par = new List<int>();
+            List<int> impar = new List<int>();
+            for (int i = 0; i < lista.Length; i++)
+            {
+                num = Convert.ToInt32(lista[i]);
+                if (num % 2 == 0)
+                    par.Add(num);
+                else if (num % 2 !=0)
+                    impar.Add(num);
+
+            }
+
+            Dictionary<String, List<int>> response = new Dictionary<String, List<int>>();
+
+            response["pares"] = par;
+            response["impares"] = impar;
+            
+
+            return response;
+        }
+
+        private void TxtNumeros_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            bool space, numero, back;
+            space = !char.IsWhiteSpace(e.KeyChar);
+            numero = !char.IsDigit(e.KeyChar);
+            back = !char.IsControl(e.KeyChar);
+
+            if (space == true && numero == true && back == true)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            string text;
+            text = txtNumeros.Text;
+            string[] arr = text.Split(' ');
+
+            
+
+            Dictionary<String, List<int>> dictParidad = Paridad(arr);
+
+            List<int> pares = dictParidad["pares"];
+            pares.Sort();
+            List<int> impares = dictParidad["impares"];
+            impares.Reverse();
+            listBox2.DataSource = pares;
+            listBox3.DataSource = impares;
+
+            //foreach (var value in dictParidad)
+            //{
+            //    int rep = 1;
+            //    if (dictParidad.Contains(value))
+            //        dictParidad[rep]++;
+            //    else
+            //       dictParidad[value] = 1;
+            //    listBox4.Items.Add("Value" + value + "occurred" + rep + "times.");
+            //}
+
+            List<double> numimpar = new List<double>();
+            List<double> numpar = new List<double>();
+
+            for (int i = 0; i < pares.Count; i++)
+            {
+                numpar.Add(Convert.ToDouble(pares[i]));
+            }
+
+            for (int i = 0; i < impares.Count; i++)
+            {
+                numimpar.Add(Convert.ToDouble(impares[i]));
+            }
+
+            List<double> numeros = new List<double>();
+            for (int i = 0; i < arr.Length; i++) 
+            {
+                numeros.Add(Convert.ToDouble(arr[i]));
+            }
+            if (pares.Count > impares.Count)
+            {
+                txtRes.Text = "El promedio es:  " + Promedio(numeros).ToString();
+            }
+            else if (impares.Count > pares.Count)
+            {
+                txtRes.Text = "La mediana es: " + Mediana(numeros).ToString();
+            }
+            else if (impares.Count == pares.Count)
+            {
+
+                txtRes.Text = "El promedio de los pares es:  " + Promedio(numpar).ToString() + "\r\n" +
+                    "La mediana de los impares es: " + Mediana(numimpar).ToString();
+            }
+            if ((Sumatotal(numeros) % 5) == 0)
+            {
+                double ln;
+                ln = Math.Log10(Promedio(numeros));
+                txtRes.Text += "\r\n" + "El logaritmo decimal del promedio de los numeros introducidos es: " + ln;
+            }
+            if (Multtotal(numeros) > 1000)
+            {
+                double raiz;
+                double med = Mediana(numeros);
+                raiz = Math.Pow(med, (1.0/3.0));
+                txtRes.Text += "\r\n" + "La raiz cubica de la mediana de los numeros introducidos es: " + raiz;
+            }
+
+        }
+
+        private void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private double Mediana (List<double> imp)
+        {
+            imp.Sort();
+            double mediana;
+            double posicion = imp.Count / 2;
+            posicion = Math.Floor(posicion);
+            mediana = imp[Convert.ToInt32(posicion)];
+
+            return mediana;
+        }
+
+        private double Promedio (List<double> par)
+        {
+            double promedio;
+            double suma = 0;
+            for (int i = 0; i < par.Count; i++)
+            {
+                suma += par[i];
+            }
+            promedio = suma / par.Count;
+            return promedio;
+        }
+
+        private int Sumatotal(List<double> list)
+        {
+            int st = 0;
+            for (int i = 0; i < list.Count; i++) 
+            {
+                st += Convert.ToInt32(list[i]);
+            }
+            return st;
+        }
+
+        private int Multtotal(List<double> list)
+        {
+            int mt = 1;
+            for (int i = 0; i < list.Count; i++)
+            {
+                mt *= Convert.ToInt32(list[i]);
+            }
+            return mt;
+        }
+ 
+
+        }
+         
+    }
+
+       
+    
+
